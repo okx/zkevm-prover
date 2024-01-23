917916a14ec22be82e46e01a2cf91eab1fa6d31e
Dumi Loghin <dumitrel.loghin@okg.com>
2024-01-23 08:28:37 +0000

    fix cuda call

--- src/starkpil/merkleTree/merkleTreeGL.cpp
+++ src/starkpil/merkleTree/merkleTreeGL.cpp
@@ -36,9 +36,13 @@ void MerkleTreeGL::genMerkleProof(Goldilocks::Element *proof, uint64_t idx, uint
 
 void MerkleTreeGL::merkelize()
 {
-#ifdef __AVX512__
+#ifdef __USE_CUDA__
     PoseidonGoldilocks::merkletree_cuda(nodes, source, width, height);
 #else
-    PoseidonGoldilocks::merkletree_cuda(nodes, source, width, height);
+#ifdef __AVX512__
+    PoseidonGoldilocks::merkletree_avx512(nodes, source, width, height);
+#else
+    PoseidonGoldilocks::merkletree_avx(nodes, source, width, height);
+#endif
 #endif
 }
