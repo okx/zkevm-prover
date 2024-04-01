800a09a2ab312a0ee71486ec9aecf2fc94c8db63
xingqiang.yuan <xingqiang.yuan@okg.com>
2024-04-02 00:08:23 +0800

    update memory

---
+++ src/utils/memory.cpp
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+#ifndef __USE_CUDA__
+void *calloc2(uint64_t count, uint64_t size) { return calloc(count, size); }
+
+void *malloc2(uint64_t size) { return malloc(size); }
+void free2(void *ptr) { free(ptr); }
+#endif
