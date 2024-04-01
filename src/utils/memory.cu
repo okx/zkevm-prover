6502d2077790ed515e65d8b384f4c3571f32f99f
xingqiang.yuan <xingqiang.yuan@okg.com>
2024-04-02 00:06:22 +0800

    update memory

--- src/utils/memory.cu
+++ src/utils/memory.cu
@@ -18,9 +18,4 @@ void *malloc2(uint64_t size) {
 }
 
 void free2(void *ptr) { cudaFree(ptr); }
-#else
-void *calloc2(uint64_t count, uint64_t size) { return calloc(count, size); }
-
-void *malloc2(uint64_t size) { return malloc(size); }
-void free2(void *ptr) { free(ptr); }
 #endif
--- src/utils/memory.cuh
+++ src/utils/memory.cuh
@@ -4,3 +4,10 @@ void *calloc2(uint64_t count, uint64_t size);
 void *malloc2(uint64_t size);
 
 void free2(void *ptr);
+
+#ifndef __USE_CUDA__
+void *calloc2(uint64_t count, uint64_t size) { return calloc(count, size); }
+
+void *malloc2(uint64_t size) { return malloc(size); }
+void free2(void *ptr) { free(ptr); }
+#endif
