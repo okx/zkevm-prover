b529d24debfcf73cbff7e8bd5e05c1acda4da67c
xzavier <yann.sjtu@gmail.com>
2024-08-12 16:52:36 +0800

    update

--- src/starkpil/chelpers_steps_pack.hpp
+++ src/starkpil/chelpers_steps_pack.hpp
@@ -19,6 +19,16 @@ public:
     vector<uint64_t> nColsStagesAcc;
     vector<uint64_t> offsetsStages;
 
+    uint8_t *ops;
+    uint16_t *args;
+    uint8_t *storePol;
+
+    Goldilocks::Element *challenges;
+    Goldilocks::Element *challenges_ops;
+    Goldilocks::Element *numbers_;
+    Goldilocks::Element *publics;
+    Goldilocks::Element *evals;
+
     using CHelpersSteps::storePolinomials;  // Just to avoid compiation warnings
     using CHelpersSteps::loadPolinomials;   // Just to avoid compiation warnings
 
@@ -70,6 +80,46 @@ public:
         }
         nColsStagesAcc[11] = nColsStagesAcc[10] + nColsStages[10]; // xDivXSubXi
         nCols = nColsStagesAcc[11] + 6; // 3 for xDivXSubXi and 3 for xDivXSubWxi
+    }
+
+    inline virtual void prepare(StarkInfo &starkInfo, StepsParams &params, ParserArgs &parserArgs, ParserParams &parserParams) {
+        challenges = (Goldilocks::Element *)malloc(params.challenges.degree()*FIELD_EXTENSION*nrowsPack*sizeof(Goldilocks::Element));
+        Gchallenges_ops = (oldilocks::Element *)malloc(params.challenges.degree()*FIELD_EXTENSION*nrowsPack*sizeof(Goldilocks::Element));
+        for(uint64_t i = 0; i < params.challenges.degree(); ++i) {
+            for(uint64_t j = 0; j < nrowsPack; ++j) {
+                challenges[(i*FIELD_EXTENSION)*nrowsPack + j] = params.challenges[i][0];
+                challenges[(i*FIELD_EXTENSION + 1)*nrowsPack + j] = params.challenges[i][1];
+                challenges[(i*FIELD_EXTENSION + 2)*nrowsPack + j] = params.challenges[i][2];
+                challenges_ops[(i*FIELD_EXTENSION)*nrowsPack + j] = params.challenges[i][0] + params.challenges[i][1];
+                challenges_ops[(i*FIELD_EXTENSION + 1)*nrowsPack + j] = params.challenges[i][0] + params.challenges[i][2];
+                challenges_ops[(i*FIELD_EXTENSION + 2)*nrowsPack + j] = params.challenges[i][1] + params.challenges[i][2];
+            }
+        }
+
+        numbers_ = (Goldilocks::Element *)malloc(parserParams.nNumbers*nrowsPack*sizeof(Goldilocks::Element));
+        for(uint64_t i = 0; i < parserParams.nNumbers; ++i) {
+            for(uint64_t j = 0; j < nrowsPack; ++j) {
+                numbers_[i*nrowsPack + j] = Goldilocks::fromU64(parserArgs.numbers[parserParams.numbersOffset+i]);
+            }
+        }
+
+
+        publics = (Goldilocks::Element *)malloc(starkInfo.nPublics*nrowsPack*sizeof(Goldilocks::Element));
+        for(uint64_t i = 0; i < starkInfo.nPublics; ++i) {
+            for(uint64_t j = 0; j < nrowsPack; ++j) {
+                publics[i*nrowsPack + j] = params.publicInputs[i];
+            }
+        }
+
+
+        evals = (Goldilocks::Element *)malloc(params.evals.degree()*FIELD_EXTENSION*nrowsPack*sizeof(Goldilocks::Element));
+        for(uint64_t i = 0; i < params.evals.degree(); ++i) {
+            for(uint64_t j = 0; j < nrowsPack; ++j) {
+                evals[(i*FIELD_EXTENSION)*nrowsPack + j] = params.evals[i][0];
+                evals[(i*FIELD_EXTENSION + 1)*nrowsPack + j] = params.evals[i][1];
+                evals[(i*FIELD_EXTENSION + 2)*nrowsPack + j] = params.evals[i][2];
+            }
+        }
     }
 
     inline virtual void storePolinomials(StarkInfo &starkInfo, StepsParams &params, Goldilocks::Element *bufferT_, uint8_t* storePol, uint64_t row, uint64_t nrowsPack, uint64_t domainExtended) {
@@ -167,7 +217,9 @@ public:
     virtual void calculateExpressions(StarkInfo &starkInfo, StepsParams &params, ParserArgs &parserArgs, ParserParams &parserParams){
         printf("into pack calculateExpressions...\n");
         bool domainExtended = parserParams.stage > 3 ? true : false;
-        uint64_t domainSize = domainExtended ? 1 << starkInfo.starkStruct.nBitsExt : 1 << starkInfo.starkStruct.nBits;   
+        uint64_t domainSize = domainExtended ? 1 << starkInfo.starkStruct.nBitsExt : 1 << starkInfo.starkStruct.nBits;
+        setBufferTInfo(starkInfo, parserParams.stage);
+        prepare(starkInfo, params, parserArgs, parserParams);
         calculateExpressionsRows(starkInfo, params, parserArgs, parserParams, 0, domainSize);
     }
     
@@ -176,10 +228,9 @@ public:
     
         bool domainExtended = parserParams.stage > 3 ? true : false;
         uint64_t domainSize = domainExtended ? 1 << starkInfo.starkStruct.nBitsExt : 1 << starkInfo.starkStruct.nBits;
-        uint8_t *ops = &parserArgs.ops[parserParams.opsOffset];
-        uint16_t *args = &parserArgs.args[parserParams.argsOffset];
-        uint64_t *numbers = &parserArgs.numbers[parserParams.numbersOffset];
-        uint8_t *storePol = &parserArgs.storePols[parserParams.storePolsOffset];
+        ops = &parserArgs.ops[parserParams.opsOffset];
+        args = &parserArgs.args[parserParams.argsOffset];
+        storePol = &parserArgs.storePols[parserParams.storePolsOffset];
 
         if(rowEnd < rowIni || rowEnd > domainSize) {
             zklog.info("Invalid range for rowIni and rowEnd");
@@ -206,45 +257,6 @@ public:
         printf("parserParams.nNumbers:%u\n", parserParams.nNumbers);
         printf("starkInfo.nPublics:%lu\n", starkInfo.nPublics);
         printf("params.evals.degree():%lu\n", params.evals.degree());
-
-        setBufferTInfo(starkInfo, parserParams.stage);
-        Goldilocks::Element challenges[params.challenges.degree()*FIELD_EXTENSION*nrowsPack];
-        Goldilocks::Element challenges_ops[params.challenges.degree()*FIELD_EXTENSION*nrowsPack];
-        for(uint64_t i = 0; i < params.challenges.degree(); ++i) {
-            for(uint64_t j = 0; j < nrowsPack; ++j) {
-                challenges[(i*FIELD_EXTENSION)*nrowsPack + j] = params.challenges[i][0];
-                challenges[(i*FIELD_EXTENSION + 1)*nrowsPack + j] = params.challenges[i][1];
-                challenges[(i*FIELD_EXTENSION + 2)*nrowsPack + j] = params.challenges[i][2];
-                challenges_ops[(i*FIELD_EXTENSION)*nrowsPack + j] = params.challenges[i][0] + params.challenges[i][1];
-                challenges_ops[(i*FIELD_EXTENSION + 1)*nrowsPack + j] = params.challenges[i][0] + params.challenges[i][2];
-                challenges_ops[(i*FIELD_EXTENSION + 2)*nrowsPack + j] = params.challenges[i][1] + params.challenges[i][2];
-            }
-        }
-
-        Goldilocks::Element numbers_[parserParams.nNumbers*nrowsPack];
-        for(uint64_t i = 0; i < parserParams.nNumbers; ++i) {
-            for(uint64_t j = 0; j < nrowsPack; ++j) {
-                numbers_[i*nrowsPack + j] = Goldilocks::fromU64(numbers[i]);
-            }
-        }
-
-
-        Goldilocks::Element publics[starkInfo.nPublics*nrowsPack];
-        for(uint64_t i = 0; i < starkInfo.nPublics; ++i) {
-            for(uint64_t j = 0; j < nrowsPack; ++j) {
-                publics[i*nrowsPack + j] = params.publicInputs[i];
-            }
-        }
-
-
-        Goldilocks::Element evals[params.evals.degree()*FIELD_EXTENSION*nrowsPack];
-        for(uint64_t i = 0; i < params.evals.degree(); ++i) {
-            for(uint64_t j = 0; j < nrowsPack; ++j) {
-                evals[(i*FIELD_EXTENSION)*nrowsPack + j] = params.evals[i][0];
-                evals[(i*FIELD_EXTENSION + 1)*nrowsPack + j] = params.evals[i][1];
-                evals[(i*FIELD_EXTENSION + 2)*nrowsPack + j] = params.evals[i][2];
-            }
-        }
 
 //        writeDataToFile("challenges.txt", (uint64_t *)challenges, params.challenges.degree()*FIELD_EXTENSION*nrowsPack);
 //        writeDataToFile("challenges_ops.txt", (uint64_t *)challenges_ops, params.challenges.degree()*FIELD_EXTENSION*nrowsPack);
