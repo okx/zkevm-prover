204125c800b5ac836e7bf05e745f61b7327fb8ee
xzavier <yann.sjtu@gmail.com>
2024-08-14 14:18:34 +0800

    update

--- src/starkpil/chelpers_steps_pack.cu
+++ src/starkpil/chelpers_steps_pack.cu
@@ -379,9 +379,6 @@ __global__ void storePolinomialsGPU(CHelpersStepsPackGPU *cHelpersSteps) {
                             for (uint64_t r = 0; r < nrowsPack; r++) {
                                 if (k * subDomainSize + row * dim + i + r*dim == 15) {
                                     printf("storePolinomialsGPU, s:%lu, r:%lu, k:%lu, dim:%lu, i:%lu, value:%lu\n", s, r, k, dim, i, uint64_t(buffT[i*nrowsPack+j]));
-                                    for (uint64_t j = 0; j < nrowsPack; j++) {
-                                        printf("%lu\n", );
-                                    }
                                 }
                             }
 
